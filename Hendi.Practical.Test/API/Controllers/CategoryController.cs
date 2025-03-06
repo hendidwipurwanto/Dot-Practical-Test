84b2140d5a8221d3f571a265823329f2977d3629
hendidwipurwanto <hendi.purwanto@gustodian.com>
2025-03-06 21:06:13 +0700

    Add api category controller

---
+++ Hendi.Practical.Test/API/Controllers/CategoryController.cs
@@ -0,0 +1,64 @@
+﻿using Application.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _categoryService.GetCategoriesAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(id);
+            return category != null ? Ok(category) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            try
+            {
+                await _categoryService.AddCategoryAsync(category);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+
+
+
+            return CreatedAtAction(nameof(GetById), new { id = category.Id }, category);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Category category)
+        {
+            if (id != category.Id) return BadRequest();
+            await _categoryService.UpdateCategoryAsync(category);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+            return NoContent();
+        }
+    }
+}
